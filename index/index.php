68b94547f254863e43ee859dcfe9f4aea986d8b3
annaSokolova88 <0997136369@bk.ru>
2017-01-24 20:45:02 +0200

    Create index.php

---
+++ index/index.php
@@ -0,0 +1,24 @@
+<?php
+class Animal{
+	protected $name;
+	
+	public function __construct($name) {
+		$this->name = $name;
+	}
+	
+	public function getName(){
+		return $this->name;
+	}
+}
+
+class Cat extends Animal{
+	
+	public function meow(){
+		return "Cat {$this->name} is saying meow";
+	}
+}
+	
+$cat = new Cat ('garfield');
+var_dump($cat->getName() === 'garfield'); 
+var_dump($cat->meow() === 'Cat garfield is saying meow');   
+							 
